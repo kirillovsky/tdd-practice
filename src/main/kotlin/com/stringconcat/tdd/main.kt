273fd020173772dfcb4c595bb186e84f3e3c9959
AlexKirillov <Kirillov.A.Valer@sberbank.ru>
2021-06-02 16:46:45 +0300

    remove useless todos

--- src/main/kotlin/com/stringconcat/tdd/main.kt
+++ src/main/kotlin/com/stringconcat/tdd/main.kt
@@ -1,9 +1,3 @@
 fun main() {
     println("Hello World!")
 }
-
-// to big amount (more than double)
-// Money should multiply to double with round to 4 decimal places
-
-// CurrencyExchanger. what if pass two rates with same currencyPairs
-// CurrencyExchanger. reversed rate
