5a574c1fde08b6329bcc2ea6743541760c93200e
AlexKirillov <Kirillov.A.Valer@sberbank.ru>
2021-06-02 09:23:04 +0300

    update todo list

--- src/main/kotlin/com/stringconcat/tdd/main.kt
+++ src/main/kotlin/com/stringconcat/tdd/main.kt
@@ -2,8 +2,9 @@ fun main() {
     println("Hello World!")
 }
 
-// 2 CHF + 4 USD = 5 USD (if rate 4:1)
-// 2 CHF + 4 USD = 10 CHF (if rate 2:1)
 // 2 EUR + 4 USD = 8 USD
 // to big amount (more than double)
 // conversion with fractional part round to 4 decimal places
+// Wallet(dollar(30), franc(20)) = Wallet(franc(20), dollar(30))
+// Wallet(dollar(30)) + franc(20) = Wallet(dollar(30), franc(20))
+// Wallet(dollar(30), franc(20)) + franc(20) = Wallet(dollar(30), franc(40))
